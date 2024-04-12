3a5763f5c98992158f50e47d3a3a73f0708ad155
Aditya Jangam <103239229+Aaditya17032002@users.noreply.github.com>
2024-04-12 22:42:26 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,41 @@
+import streamlit as st
+
+def main():
+    st.title("Trip Itinerary")
+
+    st.subheader("Day 1: May 2")
+    st.write("- Leave for Mumbai at 4am")
+    st.write("- Reach Mumbai at 9:30")
+    st.write("- Take bus for Lonavala")
+    st.write("- Reach Lonavala by 12")
+    st.write("- Check in at the villa")
+    st.write("- Chill, relax, eat")
+    st.write("- Take traveler and go about places, dine out, and come back to rest")
+
+    st.subheader("Day 2: May 3")
+    st.write("- Wake up and have breakfast")
+    st.write("- Book traveler, move around, have lunch, and come back")
+    st.write("- Take rest")
+    st.write("- Take traveler, move about, have dinner, and come home to rest")
+
+    st.subheader("Day 3: May 4")
+    st.write("- Wake up and have breakfast")
+    st.write("- Visit Imagicaa or Della Adventure Park or anywhere else")
+    st.write("- Come home and rest")
+
+    st.subheader("Day 4: May 5")
+    st.write("- Check out from villa")
+    st.write("- Take local train to Mumbai railway station")
+    st.write("- Book one or two rooms")
+    st.write("- Put bags there and go out")
+    st.write("- 1st: Eat something")
+    st.write("- 2nd: Siddhi Vinayak Mandir")
+    st.write("- 3rd: Anywhere else")
+    st.write("- 4th: In the evening, go to Marine Drive")
+    st.write("- 5th: Have dinner somewhere")
+    st.write("- Go back to rooms in railway station, change, and board the train back home")
+    st.write("Trip over! 👍🏻")
+
+if __name__ == "__main__":
+    main()
+  
